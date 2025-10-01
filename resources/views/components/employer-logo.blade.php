d0d453affa5905659b2d591e0cb2387abaeee0c3
wsydn <wsydney@web.de>
2025-10-01 16:46:12 +0200

    Enhance input styling for file uploads and improve employer logo display

--- resources/views/components/employer-logo.blade.php
+++ resources/views/components/employer-logo.blade.php
@@ -1,3 +1,3 @@
 @props(['employer', 'width' => 90])
 
-<img src="{{ asset($employer->logo) }}" alt="" class="rounded-xl bg-white dark:bg-black" width="{{ $width }}">
+<img src="{{ asset($employer->logo) }}" alt="" class="rounded-xl bg-transparent dark:bg-black object-contain aspect-square" width="{{ $width }}">
