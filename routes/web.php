330d1c478f9b82ec903603e85e0a2213a28afad1
wsydn <wsydney@web.de>
2025-10-03 11:50:51 +0200

    Refactor route definitions for improved organization and readability

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,10 @@ use App\Http\Controllers\SessionController;
 use App\Livewire\SearchJobs;
 use Illuminate\Support\Facades\Route;
 
+// Routes that do not require authentication ==============================================================
+
 Route::get('/', [JobController::class, 'index'])
     ->name('jobs.index');
-
-Route::get('/jobs/create', [JobController::class, 'create'])
-    ->middleware('auth');
-
-Route::post('/jobs', [JobController::class, 'store'])
-    ->middleware('auth');
 
 Route::get('/jobs/search', SearchJobs::class)
     ->defaults('facetMethod', 'query')
@@ -23,38 +19,55 @@ Route::get('/jobs/search', SearchJobs::class)
 Route::get('/jobs/{job}', [JobController::class, 'show'])
     ->name('jobs.show');
 
+Route::get('/employers', [EmployerController::class, 'index'])
+    ->name('employers.index');
+
+// Routes that require authentication =====================================================================
 
 Route::middleware('auth')->group(function() {
+    // Job creation and management
+    Route::get('/jobs/create', [JobController::class, 'create']);
+
+    Route::post('/jobs', [JobController::class, 'store']);
+
     Route::get('/jobs/{job}/edit', [JobController::class, 'edit'])
         ->can('edit', 'job')
         ->name('jobs.edit');
 
     Route::patch('/jobs/{job}', [JobController::class, 'update'])
-        ->name('jobs.update')
-        ->can('edit', 'job');
+        ->can('edit', 'job')
+        ->name('jobs.update');
 
     Route::delete('/jobs/{job}/destroy', [JobController::class, 'destroy'])
-        ->name('jobs.destroy')
-        ->can('edit', 'job');
-});
+        ->can('edit', 'job')
+        ->name('jobs.destroy');
 
-Route::middleware('guest')->group(function() {
-    Route::get('/register', [RegisteredUserController::class, 'create']);
-    Route::post('/register', [RegisteredUserController::class, 'store']);
-
-    Route::get('/login', [SessionController::class, 'create'])->name('login');
-    Route::post('/login', [SessionController::class, 'store']);
-});
-
-Route::delete('/logout', [SessionController::class, 'destroy'])->middleware('auth');
-
-Route::get('/employers', [EmployerController::class, 'index'])->name('employers.index');
-
-Route::middleware('auth')->group(function() {
+    // Employer management
     Route::get('/employers/{employer}/edit', [EmployerController::class, 'edit'])
         ->can('edit', 'employer')
         ->name('employers.edit');
+
     Route::patch('/employers/{employer}', [EmployerController::class, 'update'])
         ->can('edit', 'employer')
         ->name('employers.update');
+
+    // Logout
+    Route::delete('/logout', [SessionController::class, 'destroy']);
+});
+
+// Routes that require the user to be a guest (not authenticated) ===============================================
+
+Route::middleware('guest')->group(function() {
+
+    // Registration management
+    Route::get('/register', [RegisteredUserController::class, 'create']);
+
+    Route::post('/register', [RegisteredUserController::class, 'store']);
+
+    // Login management
+    Route::get('/login', [SessionController::class, 'create'])
+        ->name('login');
+
+    Route::post('/login', [SessionController::class, 'store'])
+        ->name('login.store');
 });
