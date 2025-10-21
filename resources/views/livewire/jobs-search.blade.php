ad53769f8a87bca84925977e0bcfe1fadf9aa3f2
wsydn <wsydney@web.de>
2025-10-21 14:35:17 +0200

    Rename Livewire components and update references in jobs-search.blade.php

--- resources/views/livewire/jobs-search.blade.php
+++ resources/views/livewire/jobs-search.blade.php
@@ -10,35 +10,35 @@
             {{ $facetMethod == 'all' ? 'wire:ignore' : '' }}
             class="mb-4 grid items-end gap-4 sm:grid-cols-12"
         >
-            <x-livewire.select
+            <x-job-searchform.select
                 class="col-span-4 md:col-span-2"
                 label="Employer"
                 name="employer"
                 :options="$employers"
             />
 
-            <x-livewire.select
+            <x-job-searchform.select
                 class="col-span-4 md:col-span-2"
                 label="Tag"
                 name="tag"
                 :options="$tags"
             />
 
-            <x-livewire.input
+            <x-job-searchform.input
                 class="col-span-4"
                 label="Search title/description"
                 name="search"
                 placeholder="Search jobs... (min. 3 char.)"
             />
 
-            <x-livewire.select
+            <x-job-searchform.select
                 class="col-span-4 md:col-span-2"
                 label="Sort"
                 name="sort"
                 :options="$sortOptions"
             />
 
-            <x-livewire.button
+            <x-job-searchform.button
                 class="col-span-4 mt-4 md:col-span-2"
                 disabled="$wire.search.length < {{ $minSearchLength }} && !$wire.employer && !$wire.tag && $wire.sort == 'title'"
                 label="Reset"
