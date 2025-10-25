17659ff4ce677f436e57559ba582f8157f54814e
wsydn <wsydney@web.de>
2025-10-25 14:59:07 +0200

    Remove unnecessary comments from fullpage-demo.blade.php for improved code clarity

--- resources/views/livewire/fullpage-demo.blade.php
+++ resources/views/livewire/fullpage-demo.blade.php
@@ -5,11 +5,6 @@
         Search in title/description (press enter to perform search)
     </label>
 
-    {{--
-        Search input:
-        - wire:model="search" keeps the input value in sync with the Livewire $search property
-        - wire:keydown.enter="performSearch" triggers performSearch() on the server to refresh results
-    --}}
     <input
         class="bg-border-black/10 w-full rounded-lg border bg-black/10 px-3 py-2 text-sm dark:border-gray-700 dark:bg-gray-800 dark:text-white"
         id="search"
@@ -19,7 +14,6 @@
         wire:keydown.enter="performSearch"
     />
 
-    {{-- Small helper/tips component for searching --}}
     <x-search-tips class="mt-4"/>
 
     @if ($jobs->count())
@@ -28,13 +22,11 @@
                 <x-jobs.card-wide :$job context="demo"/>
             @endforeach
 
-            {{-- Pagination links (Livewire-compatible) --}}
             <div>
                 {{ $jobs->links() }}
             </div>
         </div>
     @else
-        {{-- Empty state: show message depending on whether a search term exists --}}
         <div class="mt-6 text-gray-500">
             {{ $search ? "No jobs found for $search" : 'Enter a search term' }}
         </div>
