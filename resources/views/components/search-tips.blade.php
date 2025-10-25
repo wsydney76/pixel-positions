9e580caee9ef31f3fdfdd8f37dd62c8b160a6d86
wsydn <wsydney@web.de>
2025-10-25 13:09:27 +0200

    Improve search tips component layout and examples for better clarity

--- resources/views/components/search-tips.blade.php
+++ resources/views/components/search-tips.blade.php
@@ -1,15 +1,23 @@
+<div {{ $attributes->merge(['class' => 'mb-4 text-sm']) }}
+     x-data="{ open: false }">
 
-<div {{ $attributes->merge(['class' => 'mb-4 text-sm']) }} x-data="{ open: false }">
-  <button type="button" @click="open = !open" class="text-sm focus:outline-none cursor-pointer">
-      <b>💡 Search Tips</b>
-  </button>
-  <div x-show="open" x-transition x-cloak class="pl-6 mt-2">
-    <b>"phrase"</b> → exact phrase<br>
-    <b>+word</b> → must include<br>
-    <b>-word</b> → exclude<br>
-    <b>word*</b> → partial match (e.g. data, database)<br>
-    <b>Words with spaces</b> → any match<br><br>
-    <b>Examples:</b> "error log", +mysql -oracle, data*
-  </div>
+    <button class="cursor-pointer text-sm focus:outline-none"
+            type="button"
+            @click="open = !open">
+        <b>💡 Search Tips</b>
+    </button>
+
+    <div class="mt-2 pl-6"
+         x-show="open"
+         x-transition
+         x-cloak>
+        <b>"phrase"</b> → exact phrase<br>
+        <b>+word</b> → must include<br>
+        <b>-word</b> → exclude<br>
+        <b>word*</b> → partial match (e.g. data, database)<br>
+        <b>Words with spaces</b> → any match<br><br>
+        <b>Examples:</b> "project manager", developer +craft, manager -project, dev*
+    </div>
+
 </div>
 
